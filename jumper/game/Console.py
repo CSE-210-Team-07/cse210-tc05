fef85144216b26ee08b23db2973687797d9603d5
StocktonCannon <can20034@byui.edu>
2021-05-19 14:23:30 -0600

    Console 1.1 only have displayMan() so far

---
+++ jumper/game/Console.py
@@ -0,0 +1,37 @@
+import random
+
+class Console:
+    """A code template for a computer console. The responsibility of this 
+    class of objects is to display text output.
+    
+    Stereotype:
+        Service Provider, Interfacer
+    """
+     
+    def displayMan(self, Lives):
+        """Gets text input from the user through the screen.
+
+        Args: 
+            self (Screen): An instance of Screen.
+
+        Returns:
+            Prints the Gameboard.
+        """
+        if Lives >= 4:
+            print("___")
+        if Lives >= 3:
+            print(" /___\ ")
+        if Lives >= 2:
+            print(" \   / ")
+        if Lives >= 1:
+            print("  \ /  ")
+            print("   0   ")
+            print("  /|\  ")
+            print("  / \  ")
+        elif Lives <= 0:
+            print("   x   ")
+            print("  /|\  ")
+            print("  / \  ")
+        
+
+    def displayWord(self):
