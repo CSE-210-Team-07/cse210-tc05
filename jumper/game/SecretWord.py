888b8e3e6caaeb49dd26f06bbf811632cb0f73f4
Michael Heston <m45t3rhest@gmail.com>
2021-05-20 16:43:51 -0600

    Finished all the methods.

--- jumper/game/SecretWord.py
+++ jumper/game/SecretWord.py
@@ -7,11 +7,20 @@ class SecretWord:
 		
 	
 	def guessWord(self, word):
-		
+		if word.upper() == self.secretWord:
+			self.wordProgress = self.secretWord
+			return True
 		return False
 	
 	def guessLetter(self, letter):
-		
+		letter.upper()
+		if letter in self.secretWord:
+			index = 0
+			for let in self.secretWord:
+				if let == letter:
+					self.wordProgress[index] = letter
+				index += 1
+			return True
 		return False
 	
 	def getNewWord(self, min, max):
@@ -23,7 +32,7 @@ class SecretWord:
 				
 				if len(word) > max:
 					break
-		self.secretWord = possWords[random.randrange(len(possWords))]
+		self.secretWord = (possWords[random.randrange(len(possWords))]).upper()
 	
 	def getWord(self):
 		return self.secretWord
